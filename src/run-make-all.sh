61b8f1dd80edaea892086aae8517d2843352e406
litzius <litzius@mit.edu>
2024-02-23 14:18:10 +0100

    added make file shell script

---
+++ src/run-make-all.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+thisDir=`pwd`
+if [[ `basename $thisDir` != "src" ]]; then
+	echo "current working directory is NOT a src directory. exiting for safety reasons."
+else
+	mkdir build 
+	cd build
+	echo "clearing build directory..."
+	make clean 
+	rm CMakeCache.txt 
+	rm config.h
+	
+	echo "starting compiler..."
+	if nvcc --version 2>/dev/null; then
+		echo ""
+		echo "CUDA found. trying to compile for CPU and GPU..."
+		# double compiling necessary on some systems due to issue in nvcc compiler. cannot hurt, so just doing it by default...
+		cmake .. 
+		cmake .. && make $@
+		make $@ echo "CPU compiling successful."
+		echo "trying GPU compiling..." && rm CMakeCache.txt && cmake .. -DENABLE_CUDA_64=on
+		make $@ 
+		make $@ && echo "CPU and GPU compiling successful."
+		cp magneto_cpu.py ../magnum/magneto_cpu.py 2>/dev/null
+		cp _magneto_cpu.so ../magnum/_magneto_cpu.so 2>/dev/null
+		cp magneto_cuda.py ../magnum/magneto_cuda.py 2>/dev/null
+		cp _magneto_cuda.so ../magnum/_magneto_cuda.so 2>/dev/null
+	else
+		echo "No CUDA found. compiling for CPU only..."
+		cmake .. 
+		cmake .. && make $@ 
+		make $@  && echo "CPU compiling successful."
+		cp magneto_cpu.py ../magnum/magneto_cpu.py 2>/dev/null
+		cp _magneto_cpu.so ../magnum/_magneto_cpu.so 2>/dev/null
+		cp magneto_cuda.py ../magnum/magneto_cuda.py 2>/dev/null
+		cp _magneto_cuda.so ../magnum/_magneto_cuda.so 2>/dev/null
+	fi
+fi
